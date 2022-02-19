ca0a1043890b1b550ea2d4c7a435f40d52b83424
Igor S <ermygut@gmail.com>
2022-02-19 13:49:01 +0200

    linking to the post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,14 @@
+import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
 
-function Post() {
+interface Props {
+  post: Post
+}
+
+function Post({ post }: Props) {
+  console.log(post)
   return (
     <main>
       <Header />
@@ -11,12 +18,59 @@ function Post() {
 
 export default Post
 
-export const getServerSideProps = async () => {
-  const query = `*[_type "post"]{
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
     _id,
     slug {                   
       current
     }
   }`
   const posts = await sanityClient.fetch(query)
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,                  
+    _createdAt,
+    title,
+    author-> {
+      name,
+      image
+    },
+     'comments': *[
+      _type == "comment" &&
+      post._ref == ^._id &&
+      approved ==  true],
+    description,          
+    mainImage,
+    slug,
+    body
+  }`
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, // after 60 seconds  it will update old cashed pae version
+  }
 }
